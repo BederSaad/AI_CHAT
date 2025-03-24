0149a083d63063dde0d2984498e0f75828b3c6c5
Beder Saad <saad.beder10@gmail.com>
2025-03-24 01:48:01 +0100

    Added pages and navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import Homepage from './routes/homepage/Homepage.jsx'
+import DashboardPage from './routes/dashboardpage/DashboardPage.jsx'
+import ChatPage from './routes/chatPage/ChatPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Homepage/>
+    ),
+  },
+  {
+    path: "/dashboard",
+    
+    children:[
+      {path: "/dashboard", element: <DashboardPage/> },
+      {path:"/dashboard/chats/:id",element:<ChatPage/>}
+    ],
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
