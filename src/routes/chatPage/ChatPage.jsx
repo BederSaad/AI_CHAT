667da16d96571eb02612cf18532d03897964b63a
Beder Saad <saad.beder10@gmail.com>
2025-03-28 21:57:43 +0100

    Chat dashboard Design with prompt layout

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,8 +1,44 @@
+import { useEffect } from 'react'
+import { useRef } from 'react'
 import './chatPage.css'
+import NewPrompt from '../../components/newPrompt/newPrompt';
 
 const ChatPage = () => {
+  
   return (
-    <div className='chatPage'>ChatPage</div>
+    <div className='chatPage'>
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <div className="message">Test Message from AI</div>
+          <div className="message user">Test Message from User</div>
+          <NewPrompt/>
+        
+        
+        </div>
+      </div>
+    </div>
   )
 }
 
